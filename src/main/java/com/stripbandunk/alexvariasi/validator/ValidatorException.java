0957cf0b16540650b6d0c42e0da5c0043968746b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-11-21 19:20:10 +0700

    membuat validator framework

---
+++ src/main/java/com/stripbandunk/alexvariasi/validator/ValidatorException.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
+ * 
+ *       http://stripbandunk.com/
+ * 
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stripbandunk.alexvariasi.validator;
+
+/**
+ *
+ * @author Eko Kurniawan Khannedy
+ */
+public class ValidatorException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ValidatorException(String message) {
+        super(message);
+    }
+}
