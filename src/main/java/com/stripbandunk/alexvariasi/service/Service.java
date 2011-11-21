1afc9cc5c32f5256562407e8c93a3c51d29b877e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-11-21 22:27:13 +0700

    membuat service framework

---
+++ src/main/java/com/stripbandunk/alexvariasi/service/Service.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
+ * 
+ *       http://stripbandunk.com/
+ * 
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stripbandunk.alexvariasi.service;
+
+import com.stripbandunk.alexvariasi.entity.AbstractEntity;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @param <T> entity
+ * @param <ID> id
+ * @author Eko Kurniawan Khannedy
+ */
+public interface Service<T extends AbstractEntity<?>, ID extends Serializable> {
+
+    void save(T entity);
+
+    void update(T entity);
+
+    void remove(T entity);
+
+    T find(ID id);
+
+    List<T> findAll();
+
+    List<T> search(String query);
+}
