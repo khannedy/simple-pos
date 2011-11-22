98a8834a3f0d29c8eaa62d26bcaa973028d350bd
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-11-22 11:43:06 +0700

    update abstract entity

---
+++ src/main/java/com/stripbandunk/alexvariasi/entity/AbstractAutoIdEntity.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
+ * 
+ *       http://stripbandunk.com/
+ * 
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stripbandunk.alexvariasi.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author Eko Kurniawan Khannedy
+ */
+@MappedSuperclass
+public class AbstractAutoIdEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AbstractAutoIdEntity other = (AbstractAutoIdEntity) obj;
+        if (this.getId() != other.getId() && (this.getId() == null
+                || !this.getId().equals(other.getId()))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+}
--- src/main/java/com/stripbandunk/alexvariasi/entity/AbstractTransactionEntity.java
+++ src/main/java/com/stripbandunk/alexvariasi/entity/AbstractTransactionEntity.java
@@ -7,12 +7,8 @@
  */
 package com.stripbandunk.alexvariasi.entity;
 
-import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,14 +18,9 @@ import javax.persistence.TemporalType;
  * @author Eko Kurniawan Khannedy
  */
 @MappedSuperclass
-public class AbstractTransactionEntity implements Serializable {
+public class AbstractTransactionEntity extends AbstractAutoIdEntity {
 
     private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Long id;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "waktu_transaksi")
@@ -38,14 +29,6 @@ public class AbstractTransactionEntity implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "waktu_transaksi_diubah")
     private Date waktuTransaksiDiubah;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     @SuppressWarnings("ReturnOfDateField")
     public Date getWaktuTransaksi() {
@@ -65,27 +48,5 @@ public class AbstractTransactionEntity implements Serializable {
     @SuppressWarnings("AssignmentToDateFieldFromParameter")
     public void setWaktuTransaksiDiubah(Date waktuTransaksiDiubah) {
         this.waktuTransaksiDiubah = waktuTransaksiDiubah;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final AbstractTransactionEntity other = (AbstractTransactionEntity) obj;
-        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 37 * hash + (this.id != null ? this.id.hashCode() : 0);
-        return hash;
     }
 }
