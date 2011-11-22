8e7c798a01d47dc93c043ba1c0ddf37a4ac59b71
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-11-22 12:36:12 +0700

    update service framework api

---
+++ src/main/java/com/stripbandunk/alexvariasi/service/DetailBarangService.java
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
+ * 
+ *       http://stripbandunk.com/
+ * 
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stripbandunk.alexvariasi.service;
+
+import com.stripbandunk.alexvariasi.entity.master.DetailBarang;
+
+/**
+ *
+ * @author Eko Kurniawan Khannedy
+ */
+public interface DetailBarangService extends Service<DetailBarang, String> {
+    
+}
---
+++ src/main/java/com/stripbandunk/alexvariasi/service/SatuanService.java
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
+ * 
+ *       http://stripbandunk.com/
+ * 
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stripbandunk.alexvariasi.service;
+
+import com.stripbandunk.alexvariasi.entity.master.Satuan;
+
+/**
+ *
+ * @author Eko Kurniawan Khannedy
+ */
+public interface SatuanService extends Service<Satuan, String> {
+    
+}
